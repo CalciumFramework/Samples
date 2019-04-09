d726fa24631c250fc100a89dde4aa2e559130b4f
Daniel Vaughan <dbvaughan@gmail.com>
2019-04-09 16:01:54 +0200

    Added new samples project for attached commands.

---
+++ Source/XamlAttachedCommands/AttachedCommandsWpf/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using Codon;
+
+namespace CommandBehaviorDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = Dependency.Resolve<ViewModel1>();
+            
+        }
+    }
+}
---
+++ Source/XamlAttachedCommands/AttachedCommandsWpf/ViewModel1.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Input;
+using Codon;
+using Codon.Services;
+using Codon.UIModel;
+using Codon.UIModel.Input;
+
+namespace CommandBehaviorDemo
+{
+	public class ViewModel1 : ViewModelBase
+	{
+		ActionCommand showDialogCommand;
+
+		public ICommand ShowDialogCommand => showDialogCommand
+				?? (showDialogCommand = new ActionCommand(ShowDialog));
+
+		void ShowDialog(object arg)
+		{
+			Dependency.Resolve<IDialogService>().ShowMessageAsync("Image clicked.");
+		}
+	}
+}
